cc5d30537f56e823887265ae6d2960269ec05764
FamidaLatheef <familatheef28@gmail.com>
2025-08-21 04:24:19 -0700

    Update Next.js config for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      console.log("DEBUG in next.config.js:", process.env.OPENAI_API_KEY);
-    }
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
 
 
 
